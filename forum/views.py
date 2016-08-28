9924daa784d4c8a72d057cf91e93ea374f70921b
Avesh Singh <aveshcsingh@gmail.com>
2016-08-28 16:54:06 -0700

    Added very simple homepage and topic page

--- forum/views.py
+++ forum/views.py
@@ -1,18 +1,22 @@
-from django.shortcuts import render
-from forum.models import Topic
+from django.shortcuts import get_object_or_404, render
 from forum.serializers import TopicSerializer
 from rest_framework import viewsets
-from django.http import HttpResponse
 from .models import Topic
 
 def index(request):
-    latest_topics = Topic.objects.order_by('-date')[:10]
-    output = ', '.join([t.name for t in latest_topics])
-    return HttpResponse(output)
+    latest_topics = Topic.objects.order_by('-name')
+    context = {
+        'latest_topics': latest_topics,
+    }
+    return render(request, 'forum/index.html', {'latest_topics': latest_topics})
+
+def topic(request, id):
+    topic = get_object_or_404(Topic, pk=id)
+    return render(request, 'forum/topic.html', {'topic': topic})
 
 class TopicViewSet(viewsets.ModelViewSet):
     """
     REST API endpoint for viewing/editing Topics.
     """
-    queryset = Topic.objects.all().order_by('date')
+    queryset = Topic.objects.all().order_by('-name')
     serializer_class = TopicSerializer
--- tiny_forum/urls.py
+++ tiny_forum/urls.py
@@ -30,4 +30,5 @@ urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
+    url(r'^topic/(?P<id>[0-9]+)/$', views.topic, name='topic')
 ]
